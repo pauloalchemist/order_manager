b5a9c217b26d44ff72f722b0277a61944efbb9ef
pauloalchemist <paulolinsdev@gmail.com>
2021-09-13 20:58:35 -0300

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_231249) do
+ActiveRecord::Schema.define(version: 2021_09_13_235234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "states", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
