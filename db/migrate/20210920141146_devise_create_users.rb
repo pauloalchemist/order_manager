66dcaa1ba0de9940f3560f8f026fb97af75e80f0
pauloalchemist <paulolinsdev@gmail.com>
2021-09-20 11:53:02 -0300

    users migrate

--- db/migrate/20210920141146_devise_create_users.rb
+++ db/migrate/20210920141146_devise_create_users.rb
@@ -4,9 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -31,7 +33,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
 
       t.timestamps null: false
     end
