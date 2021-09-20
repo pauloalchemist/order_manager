1dc29bbffa6820da8b31ed648f9116c96ffd32a0
pauloalchemist <paulolinsdev@gmail.com>
2021-09-20 20:33:20 -0300

    added devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:first_name, :last_name, :email, :password, :current_password)
-    end
+  def configure_permitted_parameters
+    added_attrs = %i[first_name last_name email password password_confirmation remember_me]
+
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
