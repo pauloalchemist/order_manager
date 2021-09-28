14dfee20c8c7987bfa4031f8b58b3dbf432fa03d
pauloalchemist <paulolinsdev@gmail.com>
2021-09-27 22:31:29 -0300

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_141146) do
+ActiveRecord::Schema.define(version: 2021_09_28_011231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2021_09_20_141146) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["states_id"], name: "index_cities_on_states_id"
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "fantasy_name", default: "", null: false
+    t.string "cnpj", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "states", force: :cascade do |t|
