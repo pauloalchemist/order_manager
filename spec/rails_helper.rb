60ba3d9a3702a4f13e306434c18072e9acff2b9a
pauloalchemist <paulolinsdev@gmail.com>
2021-10-17 18:39:31 -0300

    warden tests helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include FactoryBot::Syntax::Methods
+  config.include Warden::Test::Helpers
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
