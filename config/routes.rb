88b68951a738c8227d54f4570eb77a5e79d7bdd5
pauloalchemist <paulolinsdev@gmail.com>
2021-11-07 14:39:54 -0300

    route for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   authenticate :user do 
     resources :suppliers
+    get 'dashboard', to: 'dashboards#dash'
   end
 
   root to: 'pages#home'
