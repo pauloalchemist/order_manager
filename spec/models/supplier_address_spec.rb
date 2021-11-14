ef08432314b99b29ff22091506a0a63fd8bc7c9d
pauloalchemist <paulolinsdev@gmail.com>
2021-11-14 14:45:34 -0300

    association test with model supplier

--- spec/models/supplier_address_spec.rb
+++ spec/models/supplier_address_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SupplierAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:supplier) }
+  end
 end
