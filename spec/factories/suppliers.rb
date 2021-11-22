fdf8ad5c7326cebccaaca02877b988a8c13013a3
pauloalchemist <paulolinsdev@gmail.com>
2021-11-22 09:38:51 -0300

    removed formatted true

--- spec/factories/suppliers.rb
+++ spec/factories/suppliers.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :supplier do
     corporate_name { Faker::Company.name }
     fantasy_name { Faker::Fantasy::Tolkien.character }
-    cnpj { Faker::Company.brazilian_company_number(formatted: true) }
+    cnpj { Faker::Company.brazilian_company_number }
   end
 end
