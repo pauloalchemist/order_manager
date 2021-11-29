2ddf37e31db398ee35492171cf11cded1c676d9c
pauloalchemist <paulolinsdev@gmail.com>
2021-11-28 22:31:49 -0300

    tests for main message and img

--- spec/integrations/visit_home_spec.rb
+++ spec/integrations/visit_home_spec.rb
@@ -6,9 +6,13 @@ describe 'visit home' do
   end
 
   it 'and find main message' do
-    expect(page).to have_content('Página Principal')
+    expect(page).to have_content('Gerenciador de Pedidos')
+    expect(page).to have_content('A sua melhor forma de controlar seus pedidos.')
   end
   it 'and find navbar' do
     expect(page).to have_css('nav.navbar')
+  end
+  it 'and find main image' do
+    expect(page).to have_css('div.img-main-page')
   end
 end
