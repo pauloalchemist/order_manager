7ed8bc7f2d13822a8b53e3eeedfcd4515c4887c5
pauloalchemist <paulolinsdev@gmail.com>
2021-12-06 21:14:34 -0300

    a way to redirect user to correct page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   authenticate :user do 
     resources :suppliers, only: [:index, :show, :new, :create]
-    get 'dashboard', to: 'dashboards#dash'
+    # get 'dashboard', to: 'dashboards#dash'
+    get '/user' => "dashboards#dash", :as => :user_root
   end
+
 
   root to: 'pages#home'
 end
