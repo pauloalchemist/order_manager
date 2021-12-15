1a27031db15a28c0dab5dd0356de2da13efb1e33
pauloalchemist <paulolinsdev@gmail.com>
2021-12-15 18:09:23 -0300

    added has_many products

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -8,6 +8,7 @@ class Supplier < ApplicationRecord
   validates :corporate_name, :cnpj, presence: true
 
   has_many :supplier_addresses, inverse_of: :supplier
+  has_many :products, inverse_of: :supplier
   accepts_nested_attributes_for :supplier_addresses
 
   private
