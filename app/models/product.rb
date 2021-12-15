b6bccb7790d60ba45a341f49b69bfdc0ebd05970
pauloalchemist <paulolinsdev@gmail.com>
2021-12-15 17:08:15 -0300

    added validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   enum status: %i[active inactive], _default: :active
 
+  validates :name, :description, length: { minimum: 10 }, presence: true
+  validates :sku, presence: true
+
   belongs_to :supplier
 end
