1a01441f23a248d56bf6ae6b3bce5bcc4fdfbb4d
pauloalchemist <paulolinsdev@gmail.com>
2021-12-15 17:51:31 -0300

    added validate length for name and description

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,5 +9,7 @@ RSpec.describe Product, type: :model do
     it { should validate_presence_of(:sku) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
+    it { should validate_length_of(:name).is_at_least(5) }
+    it { should validate_length_of(:description).is_at_least(10) }
   end
 end
