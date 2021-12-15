efd14ddfe902d9199b19723791d8e7c75e5ee54d
pauloalchemist <paulolinsdev@gmail.com>
2021-12-15 17:07:33 -0300

    test validate presence

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Product, type: :model do
   describe 'associations' do
     it { should belong_to(:supplier) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:sku) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+  end
 end
