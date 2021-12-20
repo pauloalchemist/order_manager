68c60eeaaa11b2cb419aa836a49da57b26c058ad
pauloalchemist <paulolinsdev@gmail.com>
2021-12-19 22:55:47 -0300

    define a different root route for logged out users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  unauthenticated do
+    root "pages#home"
+  end
+
   authenticate :user do 
+    root 'dashboards#dash', as: :authenticated_root
+    get '/dashboard' => "dashboards#dash", :as => :user_root
     resources :suppliers, only: [:index, :show, :new, :create]
-    # get 'dashboard', to: 'dashboards#dash'
-    get '/user' => "dashboards#dash", :as => :user_root
   end
 
 
-  root to: 'pages#home'
+  # root to: 'pages#home'
 end
