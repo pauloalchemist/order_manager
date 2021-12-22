01586b095e8f866a75d3a2da75930ba9c384a259
pauloalchemist <paulolinsdev@gmail.com>
2021-12-21 22:33:59 -0300

    should render template index

--- spec/requests/suppliers_spec.rb
+++ spec/requests/suppliers_spec.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Suppliers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Suppliers', type: :request do
+  let(:user) { create(:user) }
+
+  describe 'GET suppliers#index' do
+    before do
+      sign_in(user)
+      get suppliers_path
+    end
+    context 'when the user is logged in' do
+      it { should render_template(:index) }
+    end
   end
 end
