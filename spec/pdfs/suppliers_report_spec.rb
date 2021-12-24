6e2b0f8d1387a98b2f652f458c60435223028d48
pauloalchemist <paulolinsdev@gmail.com>
2021-12-24 12:28:46 -0300

    contains corporate name

---
+++ spec/pdfs/suppliers_report_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe SuppliersReportPdf do
+  context 'Gerar pdf' do
+    let(:suppliers) { [build(:supplier)] }
+    context 'render pdf content' do
+      let(:pdf) { SuppliersReportPdf.new(suppliers) }
+      let(:pdf_content) { PDF::Reader.new(StringIO.new(pdf.render)).page(1).to_s }
+
+      it 'contains corporate company' do
+        expect(pdf_content).to include(suppliers[0][:corporate_name])
+      end
+    end
+  end
+end
