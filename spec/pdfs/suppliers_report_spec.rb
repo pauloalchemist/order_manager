82dee4b9c6da8007653c44b50c462f6623140357
pauloalchemist <paulolinsdev@gmail.com>
2021-12-24 12:48:10 -0300

    improved test descriptions

--- spec/pdfs/suppliers_report_spec.rb
+++ spec/pdfs/suppliers_report_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe SuppliersReportPdf do
-  context 'Gerar pdf' do
+  context 'when rendering pdf' do
     let(:suppliers) { [build(:supplier)] }
-    context 'render pdf content' do
+    context 'should have in the report' do
       let(:pdf) { SuppliersReportPdf.new(suppliers) }
       let(:pdf_content) { PDF::Reader.new(StringIO.new(pdf.render)).page(1).to_s }
 
-      it 'contains corporate company' do
+      it 'field with corporate name of the company' do
         expect(pdf_content).to include(suppliers[0][:corporate_name])
       end
-      it 'contains fantasy company' do
+      it 'field with fantasy name of the company' do
         expect(pdf_content).to include(suppliers[0][:fantasy_name])
       end
       it 'field with cnpj of the company' do
