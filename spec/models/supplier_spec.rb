c6ecb72b28028ef0c18f0159adff9a3ee2ecf2c0
pauloalchemist <paulolinsdev@gmail.com>
2021-12-27 20:59:11 -0300

    contextos de teste aprimorados

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -9,27 +9,27 @@ RSpec.describe Supplier, type: :model do
     it { expect(supplier).to be_instance_of(Supplier) }
     it { expect(supplier).to be_valid }
     it { expect(supplier.active?).to be true }
+  end
 
-    describe 'validations' do
-      it { should validate_presence_of(:corporate_name) }
-      it { should validate_presence_of(:cnpj) }
+  context 'validations' do
+    it { should validate_presence_of(:corporate_name) }
+    it { should validate_presence_of(:cnpj) }
+  end
+
+  context 'associations' do
+    it { should have_many(:supplier_addresses) }
+    it { should have_many(:products) }
+    it { expect(supplier).to accept_nested_attributes_for(:supplier_addresses) }
+  end
+
+  context 'supplier is invalid' do
+    it 'if cnpj is invalid' do
+      supplier.cnpj = '00.000.000/0000-00'
+      expect(supplier).to be_invalid
     end
-
-    describe 'associations' do
-      it { should have_many(:supplier_addresses) }
-      it { should have_many(:products) }
-      it { expect(supplier).to accept_nested_attributes_for(:supplier_addresses) }
-    end
-
-    context 'supplier is invalid' do
-      it 'if cnpj is invalid' do
-        supplier.cnpj = '00.000.000/0000-00'
-        expect(supplier).to be_invalid
-      end
-      it 'if corporate name is blank' do
-        supplier.corporate_name = ''
-        expect(supplier).to be_invalid
-      end
+    it 'if corporate name is blank' do
+      supplier.corporate_name = ''
+      expect(supplier).to be_invalid
     end
   end
 end
