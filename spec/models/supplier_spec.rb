78b206593290774e51b4a94bef5eac52433227e1
pauloalchemist <paulolinsdev@gmail.com>
2021-12-27 13:35:33 -0300

    expect supplier be instance of Suppliers

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
   let(:supplier) { build(:supplier) }
 
   context 'test supplier' do
+    it { expect(supplier).to be_instance_of(Supplier) }
     it { expect(supplier).to be_valid }
     it { expect(supplier.active?).to be true }
 
