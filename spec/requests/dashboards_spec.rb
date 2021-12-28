33f616c076f4c0aaa7c43f1ad9cfa46250d291ce
pauloalchemist <paulolinsdev@gmail.com>
2021-12-28 13:11:08 -0300

    tests for dashboards#dash

--- spec/requests/dashboards_spec.rb
+++ spec/requests/dashboards_spec.rb
@@ -1,7 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Dashboards", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Dashboards', type: :request do
+  let(:user) { create(:user) }
+
+  describe 'GET dashboards#dash' do
+    before do
+      sign_in(user)
+      get '/dashboard'
+    end
+
+    context 'when the user is logged in' do
+      it { should render_template(:dash) }
+
+      it 'should response status code 200' do
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'when the user is logged out' do
+      it 'should redirect to login page for anonymous user' do
+        sign_out(user)
+        get '/dashboard'
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 end
