e645827eb7fd6fd79441063199fa9bd5a65a027d
pauloalchemist <paulolinsdev@gmail.com>
2022-01-03 21:28:30 -0300

    supplier_address factory working

--- spec/factories/supplier_addresses.rb
+++ spec/factories/supplier_addresses.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :supplier_address do
-    
+    address { Faker::JapaneseMedia::DragonBall.character }
+    number { 12 }
+    district { Faker::JapaneseMedia::DragonBall.planet }
+    cities_id { 1 }
+    states_id { 25 }
+    zipcode { '44100-000' }
+
+    association :supplier
   end
 end
