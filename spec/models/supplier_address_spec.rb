56f2548c880783d861c4b68cd0901797d27799ef
pauloalchemist <paulolinsdev@gmail.com>
2022-01-03 21:29:47 -0300

    tests for valid factory and model validations

--- spec/models/supplier_address_spec.rb
+++ spec/models/supplier_address_spec.rb
@@ -1,7 +1,19 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SupplierAddress, type: :model do
-  describe 'associations' do
+  let(:supplier_address) { build(:supplier_address) }
+
+  context 'valid supplier_address factory' do
+    it { expect(supplier_address).to be_valid }
+  end
+
+  context 'associations' do
     it { should belong_to(:supplier) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:address) }
   end
 end
