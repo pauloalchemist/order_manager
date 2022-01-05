99fc6e3b14a5893a39cf840a15b6aaa304634ea5
pauloalchemist <paulolinsdev@gmail.com>
2022-01-05 16:57:15 -0300

    test price_list factory

--- spec/models/price_list_spec.rb
+++ spec/models/price_list_spec.rb
@@ -3,6 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe PriceList, type: :model do
+  let(:price_list) { build(:price_list) }
+
+  context 'test price_list factory' do
+    it { expect(price_list).to be_valid }
+  end
+
   describe 'associations' do
     it { should belong_to(:product) }
   end
