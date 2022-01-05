e8a89894816259c14837350a8ec3d7ef7cfcfeb2
pauloalchemist <paulolinsdev@gmail.com>
2022-01-05 17:40:06 -0300

    simple password complexity requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
+
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+    errors.add :password,
+               'Senha deve possuir: 1 letra maíscula, 1 letra minúscula, 1 digito e 1 caracter especial.'
+  end
 end
