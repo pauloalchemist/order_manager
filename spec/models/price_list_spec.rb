e50f15f87dca50937ad798e81083e8a4912e5fe6
pauloalchemist <paulolinsdev@gmail.com>
2022-01-05 17:07:11 -0300

    price_list factory is invalid if price equals 0.00

--- spec/models/price_list_spec.rb
+++ spec/models/price_list_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe PriceList, type: :model do
     it { expect(price_list).to be_valid }
   end
 
+  context 'price_list factory is invalid' do
+    it 'if price equals 0.00' do
+      price_list.price = 0.00
+      expect(price_list).to be_invalid
+    end
+  end
+
   context 'associations' do
     it { should belong_to(:product) }
   end
