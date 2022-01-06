96a17b482a94ddf62436085bf08f80e504b0f72e
pauloalchemist <paulolinsdev@gmail.com>
2022-01-06 10:52:30 -0300

    factory product

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :product do
-    supplier { nil }
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph }
+    sku { Faker::Lorem.characters(number: 6) }
+
+    association :supplier
   end
 end
