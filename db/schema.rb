129735f85af505a9ddbf5d5f06dd2dc589d0a185
pauloalchemist <paulolinsdev@gmail.com>
2022-01-12 21:18:17 -0300

    added column image into to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_150156) do
+ActiveRecord::Schema.define(version: 2022_01_12_201928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_01_05_150156) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0, null: false
+    t.string "image"
     t.index ["supplier_id"], name: "index_products_on_supplier_id"
   end
 
