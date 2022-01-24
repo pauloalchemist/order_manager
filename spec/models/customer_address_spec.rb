188e15d1fe4e4cfcf24dcc27fafdaa2f31912aa5
pauloalchemist <paulolinsdev@gmail.com>
2022-01-24 11:39:26 -0300

    aded test for validations

--- spec/models/customer_address_spec.rb
+++ spec/models/customer_address_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe CustomerAddress, type: :model do
   context 'test customer_address factory' do
     it { expect(customer_address).to be_valid }
   end
+
+  context 'validations' do
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:number) }
+    it { should validate_presence_of(:district) }
+    it { should validate_presence_of(:zipcode) }
+  end
 end
