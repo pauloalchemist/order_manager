6e068cf99073075bbd4b2f27c3b76c699c2fd3a9
pauloalchemist <paulolinsdev@gmail.com>
2022-01-24 11:15:02 -0300

    added deṕendent delete_all

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class Customer < ApplicationRecord
   enum status: %i[active inactive], _default: :active
 
   validates :name, :cnpj, :email, presence: true
 
-  has_many :customer_addresses, inverse_of: :customer
+  has_many :customer_addresses, inverse_of: :customer, dependent: :delete_all
   accepts_nested_attributes_for :customer_addresses
 end
